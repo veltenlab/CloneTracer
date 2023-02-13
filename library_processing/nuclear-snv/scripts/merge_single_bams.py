7a7d51f889ba08fa51b9ba0e948e6c55fa94de46
Sergi Beneyto Calabuig <sbeneyto@CZC0018ZYC.crg.es>
2023-02-13 14:18:12 +0100

    Fix merge bam scripts for 1 core

--- library_processing/nuclear-snv/scripts/merge_single_bams.py
+++ library_processing/nuclear-snv/scripts/merge_single_bams.py
@@ -4,6 +4,7 @@ import gzip
 import pandas as pd
 import os
 import glob
+import re
 import math
 import multiprocessing
 
@@ -27,6 +28,12 @@ cores = args.cores
 max_files = args.max_files-cores-2
 max_files_samtools = 996
 keep_temp = args.keep_temp
+
+if args.max_files-cores-2 <= 1:
+    max_files = args.max_files
+
+else:
+    max_files = args.max_files-cores-2
 
 # function to merge single cell bam files
 def merge_bams(bam_list, merged_bam):
@@ -60,8 +67,6 @@ for file in initial_bams:
 
 bams_list = []
 
-print(len(final_bams))
-
 # if the total barcodes < max open files i just distribute the barcodes in the cores
 if len(final_bams) < max_files and len(final_bams)/cores < max_files_samtools:
 
@@ -88,6 +93,17 @@ if len(final_bams) < max_files and len(final_bams)/cores < max_files_samtools:
     # run parallel process
     with multiprocessing.Pool(cores) as pool:
         pool.starmap(merge_bams, zip(bams_list, temp_bams))
+
+    # create shell string to merge intermediate files into final BAM file
+    shell_merge = "samtools merge -n" + " " + merged_bam + " " + " ".join(temp_bams)
+
+    # execute merge command
+    os.system(shell_merge)
+
+    # remove intermediate BAM files
+    rm = "rm " + " ".join(temp_bams)
+
+    os.system(rm)
 
 # if there are more barcodes than max open files more iterations need to be done
 elif len(final_bams) > max_files and max_files_samtools*cores < max_files or len(final_bams) < max_files and max_files_samtools*cores < max_files:
@@ -141,26 +157,50 @@ elif len(final_bams) > max_files and max_files_samtools*cores < max_files or len
             with multiprocessing.Pool(cores) as pool:
                 pool.starmap(merge_bams, zip(bams_list[cores*i:], temp_bams[cores*i:]))
 
+    # create shell string to merge intermediate files into final BAM file
+    shell_merge = "samtools merge -n" + " " + merged_bam + " " + " ".join(temp_bams)
 
-# create shell string to merge intermediate files into final BAM file
-shell_merge = "samtools merge -n" + " " + merged_bam + " " + " ".join(temp_bams)
+    # execute merge command
+    os.system(shell_merge)
 
-# execute merge command
-os.system(shell_merge)
+    # remove intermediate BAM files
+    rm = "rm " + " ".join(temp_bams)
 
-# remove intermediate BAM files
-rm = "rm " + " ".join(temp_bams)
+    os.system(rm)
 
-os.system(rm)
+# in cases the max_files is very low or just 1 core is chosen
+else:
+
+    temp_bams = [re.sub(".bam", "",merged_bam) + "_" + str(i) + "_temp.bam" for i in range(len(final_bams)-1)]
+
+    # do as many iterations as non-empty BAM files are present
+    for i in range(len(final_bams)-1):
+
+        # first iteration is a bit different
+        if i == 0:
+
+            merge_bams(final_bams[0:1], temp_bams[i])
+
+        else:
+
+            shell_merge = "samtools merge -n " + temp_bams[i] + " " + temp_bams[i-1] + " " + final_bams[i+1]
+
+            os.system(shell_merge)
+
+    # rename last bam file which contains all the reads
+    mv_shell = "mv " + temp_bams[-1] + " " + merged_bam
+    os.system(mv_shell)
+
+    # remove intermediary bam files
+    int_bams = glob.glob("*_temp.bam")
+    rm_shell = "rm " + " ".join(int_bams)
+    os.system(rm_shell)
 
 # remove intermediate empty folders
 if not keep_temp:
 
     temp_dir = out_dir + "/temp_bams"
 
-    #rm = "rm -r " + temp_dir + "sorted " + temp_dir + "renamed " + temp_dir + "consensus"
-
     rm = "rm -r " + temp_dir
-    
-    os.system(rm)
 
+    os.system(rm)
